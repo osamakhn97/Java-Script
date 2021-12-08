22b70d7513207d687fa47224264faa2ded65ed2a
osama <osamakhn97@gmail.com>
2021-12-08 17:49:30 +0500

    functions

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,8 +1,39 @@
 const startGameBtn = document.getElementById('start-game-btn');
+const paper = "PAPER";
+const rock = "ROCK";
+const scissors = "SCISSORS";
+let gameRunning = false;
+const defaultVal = "ROCK";
 
+
+function selection(){
+    
+let tool = prompt("Choose paper,Rock or Scissors",'').toUpperCase;
+if(tool !== paper &&
+tool !== rock &&
+tool !== scissors
+){
+alert(`we have selected ${defaultVal} for you`);
+return defaultVal
+    
+}
+else{
+    return tool
+}
+
+}
 const start =  ()=>{
 
+   if(gameRunning){
+       return
+   }
+   else{
     console.log("Starting......");
+    let myTool = selection();
+    alert(myTool)
+   }
+   
 }
+   
 
 startGameBtn.addEventListener('click',start);
